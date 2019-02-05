23e5d013441f1cefc0c9e5a21b55e470cb740265
AV <dragon313@mail.ru>
2019-02-05 18:33:42 +0300

    Complete hw0

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -2,7 +2,19 @@ package ru.javawebinar.topjava.util;
 
 import java.time.LocalTime;
 
+/**
+ * Утилитный класс для работы со временем
+ */
 public class TimeUtil {
+
+    /**
+     * Метод определяет находится ли дата в заданном промежутке
+     *
+     * @param lt        дата, которую нужно проверить
+     * @param startTime дата начала промежутка
+     * @param endTime   дата окончания промежутка
+     * @return возвращает {@code true} если дата входит в промежуток, иначе {@code false}
+     */
     public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
     }
