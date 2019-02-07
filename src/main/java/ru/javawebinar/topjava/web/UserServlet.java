33550c0b991cb41ee2050e6080df88fee1046e3d
Alexander Vdovenkov <dragon313@mail.ru>
2019-02-07 16:01:49 +0300

    Проект переведё на war

---
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.topjava.web;
+
+import java.io.IOException;
+
+/**
+ * @author Admin
+ * Date: 07.02.2019
+ */
+public class UserServlet extends javax.servlet.http.HttpServlet {
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
